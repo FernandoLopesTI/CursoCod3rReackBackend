8b1c8a9462fc8a5dd5bd42f4c9aaf1d3cb42a4b1
Fernando Lopes <nandolopis@gmail.com>
2018-12-18 17:11:41 -0200

    Configurando o Servidor com o Express

---
+++ src/config/server.js
@@ -0,0 +1,22 @@
+//porta do server 
+const port = 3003
+
+//responsável para fazer o parser do corpo da requisição 
+const bodyParser = require('body-parser')
+
+//servidor web
+const express = require('express')
+
+//start servidor web
+const server = express()
+
+//aplicação dos middleware  para requisição
+
+//extender o padrao urlencoded - suportar mais tipos de dados 
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json())
+
+//start porta
+server.listen(port, () => console.log(`Backend is Running on port ${port}`))
+
+
